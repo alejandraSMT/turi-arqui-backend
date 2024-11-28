98823156b4edf3447f7e77b1f303d21924efe632
frankvic03 <frankviva0307@gmail.com>
2024-11-28 01:21:37 -0500

    Done Blog Backend
    Done Blog Backend

--- services/blog-service/src/routes/BlogRoutes.js
+++ services/blog-service/src/routes/BlogRoutes.js
@@ -5,6 +5,7 @@ const BlogController = require('../controllers/BlogController');
 // Endpoint para obtener todos los comentarios por lugar
 router.get('/getComentarios', BlogController.getAllComentarios);
 
+
 // Endpoint para crear un nuevo comentario
 router.post('/createComentario', BlogController.createComentario);
 
